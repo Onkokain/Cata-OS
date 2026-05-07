0d84552f5e0f9e68d395e4977843c55eb6391574
Rupraj Baral <ruprajbaral@gmail.com>
2026-05-07 07:23:42 +0545

    operating system boots into floppy

--- src/main.asm
+++ src/main.asm
@@ -1,12 +1,16 @@
-org 0x7C00 ; bootsector where bios searches for os, 0x7c00 is for legacy booting
+; bootsector is defined so that the bios can find the operating system at address 0x7C00, org is a directive to initiate the starting
+org 0x7C00
 
 bits 16 ; emits 16 bits code, directive not instruction
 
+; main program loop starts
 main:
   hlt
 .halt:
   jmp .halt
+; main program loop ends
 
-times 510-($-$$) db 0 ; makes the number of bits =512
+times 510-($-$$) db 0 ; makes the number of bits =512 since using a floppy to boot
+
 dw 0AA55h ; for bootloader, dw=more than a single character
 
