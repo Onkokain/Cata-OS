3e8040763438ed358e897dc2ebae5842d3a216cf
Rupraj Baral <ruprajbaral@gmail.com>
2026-05-08 14:11:22 +0545

    Adding fat16 support

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,15 +1,16 @@
-; bootsector is defined so that the bios can find the operating system at address 0x7C00, org is a directive to initiate the starting
+; bootsector defines the starting address as 0x7C00, org is a directive to initiate the starting; legacy booting
 org 0x7C00
 bits 16 ; emits 16 bits code, directive not instruction
 
+ ;macro defined for a '\n' new line
 %define ENDL 0x0D,0x0A
 
 start:
-  jmp main
+  jmp main ; jumping to main since prints preceeds main
 ;
 ; Prints a string to the screen
 ;
-prints:
+prints: ; function to display text on the screen
   ; save registers to be modified
   push si
   push ax
@@ -20,7 +21,7 @@ prints:
   jz .done ; conditional jump if bitwise or returns 0
 
   mov ah, 0x0e
-  mov bh,0
+  mov bh,0 ; moves bh to 0
   int 0x10
   jmp .loop ; loops if more strings left
 
@@ -46,6 +47,7 @@ main:
 
   ; prints message
   mov si,msg_hello
+  mov si,hello
   call prints
   hlt
 
@@ -55,9 +57,10 @@ main:
 ; main program loop ends
 
 msg_hello: db "Hello World!", ENDL,0
+hello: db "Hello....", ENDL,0
 
 
-times 510-($-$$) db 0 ; makes the number of bits =512 since using a floppy to boot
 
-dw 0AA55h ; for bootloader, dw= define word; 2 bits
+times 510-($-$$) db 0 ; makes the number of bytes =512 since using a floppy to boot
 
+dw 0AA55h ; for bootloader, dw= define word; 2 bytes
