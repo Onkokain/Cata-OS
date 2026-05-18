5fba4b00fe81096ae674019c0089ec4507f0f2c4
Rupraj Baral <ruprajbaral@gmail.com>
2026-05-18 11:31:45 +0545

    implementing 16b protected mode

--- src/bootloader/stage2/entry.asm
+++ src/bootloader/stage2/entry.asm
@@ -13,6 +13,26 @@ entry:
     mov bp, sp
     sti
 
+    cli
+    call EnableA20
+    call loadGDT
+
+    mov eax,cr0
+    or al,1
+    mov cr0,eax
+
+    jmp dword 08h:.pmode
+
+.rmode:
+  mov ax,0
+  mov ds,ax
+  mov ss,ax
+
+  sti
+
+  mov si,g_HelloR
+
+
     xor dh, dh
     push dx
     ;call _cstart_
