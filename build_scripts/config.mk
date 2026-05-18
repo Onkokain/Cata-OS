87059e323a7659785c2482e88cdb7533fdd6ed5e
Rupraj Baral <ruprajbaral@gmail.com>
2026-05-18 09:28:44 +0545

    gcc and binutils configured and installing:

---
+++ build_scripts/config.mk
@@ -0,0 +1,7 @@
+BINUTILS_VERSION=2.46.0
+BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-$(BINUTILS_VERSION).tar.xz
+
+GCC_VERSION=16.1.0
+GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-$(GCC_VERSION)/gcc-$(GCC_VERSION).tar.xz
+
+TARGET=i686-elf
--- build_scripts/toolchain.mk
+++ build_scripts/toolchain.mk
@@ -1,33 +1,37 @@
+TOOLCHAIN_PREFIX=$(abspath toolchain/$(TARGET))
+
+export PATH:=$(TOOLCHAIN_PREFIX)/bin:$(PATH)
+export TARGET_CC=$(TARGET)-gcc
+export TARGET_CXX=$(TARGET)-g++
+export TARGET_LD=$(TARGET)-gcc
+
 toolchain: toolchain_binutils toolchain_gcc
 
-BINUTILS_URL=https://ftp.gnu.org/gnu/binutils/binutils-2.46.0.tar.xz
-GCC_URL=https://ftp.gnu.org/gnu/gcc/gcc-16.1.0/gcc-16.1.0.tar.xz
-TOOLCHAIN_PREFIX=toolchain/i686-elf
-TARGET=i686-elf
+BINUTILS_BUILD=toolchain/binutils-build-$(BINUTILS_VERSION)
 toolchain_binutils:
-	mkdir toolchain
+	mkdir -p toolchain
 	cd toolchain && wget $(BINUTILS_URL)
-	cd toolchain && tar -xf binutils-2.46.0.tar.xz
-	mkdir toolchain/binutils-build-2.46.0
-	cd toolchain/binutils-build-2.46.0 && ../binutils-2.46.0/configure \
+	cd toolchain && tar -xf binutils-$(BINUTILS_VERSION).tar.xz
+	mkdir -p $(BINUTILS_BUILD)
+	cd $(BINUTILS_BUILD) && ../binutils-$(BINUTILS_VERSION)/configure \
 		--prefix="$(TOOLCHAIN_PREFIX)"    \
 		--target=$(TARGET)                \
 		--with-sysroot                    \
 		--disable-nls                     \
 		--disable-werror
-	$(MAKE)-j8 -C toolchain/binutils-build-2.46.0
-	$(MAKE) -C toolchain/binutils-build-2.46.0 install
+	$(MAKE) -j8 -C $(BINUTILS_BUILD)
+	$(MAKE) -C $(BINUTILS_BUILD) install
 
+GCC_BUILD=toolchain/gcc-build-$(GCC_VERSION)
 toolchain_gcc: toolchain_binutils
-	mkdir toolchain
 	cd toolchain && wget $(GCC_URL)
-	cd toolchain && tar -xf gcc-16.1.0.tar.xz
-	mkdir toolchain/gcc-build-16.1.0
-	cd toolchain/gcc-build-16.1.0 && ../gcc-16.1.0/configure \
+	cd toolchain && tar -xf gcc-$(GCC_VERSION).tar.xz
+	mkdir -p $(GCC_BUILD)
+	cd $(GCC_BUILD) && ../gcc-$(GCC_VERSION)/configure \
 		--prefix="$(TOOLCHAIN_PREFIX)"    \
 		--target=$(TARGET)                \
 		--disable-nls                     \
-		--enable-languages=c,c++					\
+		--enable-languages=c,c++          \
 		--without-headers
-	$(MAKE)-j8 -C toolchain/binutils-build-2.46.0
-	$(MAKE) -C toolchain/binutils-build-2.46.0 install
+	$(MAKE) -j8 -C $(GCC_BUILD) all-gcc all-target-libgcc
+	$(MAKE) -C $(GCC_BUILD) install-gcc install-target-libgcc
