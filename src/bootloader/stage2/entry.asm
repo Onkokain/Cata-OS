4c483acec9d479e1ba4c297926474a75eda3d06f
Rupraj Baral <ruprajbaral@gmail.com>
2026-05-21 17:22:40 +0545

    commenting code

--- src/bootloader/stage2/entry.asm
+++ src/bootloader/stage2/entry.asm
@@ -1,31 +1,36 @@
-bits 16
+bits 16 ; defines 16 bits code to be used
 
+;defining it as the start of the program
 section .entry
 
+;externally defined labels
 extern __bss_start
 extern __end
 
-;extern _cstart_
-global entry
 
+global entry ; defining it as a global label
+
+; main program starts
 entry:
-    cli
+    cli ; disabling hardware interrupts
 
-    mov [g_BootDrive],dl
-    mov ax, ds
-    mov ss, ax
+    ;
+    mov [g_BootDrive],dl ; moves dl to address 0
+    mov ax, ds ; moves ds to ax
+    mov ss, ax ; moves ax to ss
     mov sp, 0
     mov bp, sp
-    sti
+    sti ; setups interrupt flags
 
   ; switching to 32 bits protected mode
     call EnableA20
     call LoadGDT
 
-    mov eax,cr0 ; switching to protected mode
-    or al,1
+    mov eax,cr0
+    or eax , 1
     mov cr0,eax
-    ; protected mode code segment
+
+    ; far jump into protected mode
     jmp dword 08h:.pmode
 
 .pmode:
@@ -36,26 +41,26 @@ entry:
   mov ds,ax
   mov ss,ax
 
-  ; remove uninit data
-  mov edi, __bss_start
-  mov ecx, __end
-  sub ecx,edi ; size
+  ; remove uninit data ;C equivalent code: memset(__bss_start, 0, __end - __bss_start);
+  mov edi, __bss_start ; address of start
+  mov ecx, __end ; address of end
+  sub ecx,edi ; size=(end-start)
   mov al,0
-  cld
-  rep stosb
+  cld ; clears directory flag
+  rep stosb ; repeat store string byte; stores at edi and repeats it ecx times
 
-
-  xor edx,edx
+  ; saving boot drive since it gets cleared during runtime
+  xor edx,edx ; setting edx=0
   mov dl,[g_BootDrive]
   push edx
 
-  cli
-  hlt
+  cli ; disabling hardware enterrupts
+  hlt ; end
 
 
-
-EnableA20:
-  [bits 16]
+; normally 20 address busses but a20 is defined as the 21st such that it can transfer from A19 to A01 without any error; expected behavior
+EnableA20: ; enable 20th data segment so that memories above 1mb is accessible
+  [bits 16] ; 16 bits real mode code
   call A20WaitInput
   mov al, DisableKeyboard
   out CommandPort,al
@@ -86,18 +91,21 @@ EnableA20:
 
 
 A20WaitInput:
+  [bits 16] ; 16 bits real mode code
   in al, CommandPort
-  test al,2
-  jnz A20WaitInput
-  ret
+  test al,2 ; bitwise and operator
+  jnz A20WaitInput ; jump if not zero
+  ret; returns
 
 A20WaitOutput:
-  in al, CommandPort
-  test al,1
-  jnz A20WaitOutput
+  [bits 16] ; 16 bits real mode code
+  in al, CommandPort ; read one byte from commandport and store in al
+  test al,1 ; bitwise and
+  jnz A20WaitOutput ; jump if not 0
   ret
 
-LoadGDT: ; global descriptor table
+LoadGDT: ; global descriptor
+  [bits 16] ; 16 bits real mode code
   lgdt [g_GDTDesc]
   ret
 
@@ -109,6 +117,7 @@ ReadCtrlOutputPort equ 0xD0
 WriteCtrlOutputPort equ 0xD1
 
 g_GDT:
+  [bits 16] ; 16 bits real mode code
         dq 0
 ;x32 bits code
         dw 0FFFFh
@@ -145,10 +154,12 @@ g_GDT:
     push dx
 
 g_GDTDesc:
+  [bits 16] ; 16 bits real mode code
   dw g_GDTDesc - g_GDT -1
   dd g_GDT
 
  g_BootDrive:
+  [bits 16] ; 16 bits real mode code
   db 0
 
 times 510-($-$$) db 0
--- src/bootloader/stage2/fardef.h
+++ src/bootloader/stage2/fardef.h
@@ -1,5 +1,5 @@
+// was prev used to define fat and _cdecl so that clang doesnt throw errors; not required anymore
 #pragma once
-
 #ifdef __clang__
 #define far
 #define _cdecl
